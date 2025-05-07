ed13bf3a5caf2bdfba73c539d72f1df1fd463c5f
juto-shogan <somtombonnu53@gmail.com>
2025-05-07 17:02:43 +0100

    odified

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER': 'root',
-        'PASSWORD': 'Somi@1414',
+        'PASSWORD': 'none of ur bizzzz',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
